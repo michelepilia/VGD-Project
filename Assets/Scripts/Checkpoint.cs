6802c739e56ed1301fd3baf908a5069e5d12f957
amarotto2 <marotz91@gmail.com>
2018-03-18 18:39:56 +0100

    abilitati tutti i checkpoint del deserto

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -5,10 +5,61 @@ using UnityEngine;
 public class Checkpoint : MonoBehaviour {
 
 	public MeshRenderer Checkpoint01;
+	public MeshRenderer Checkpoint02;
+	public MeshRenderer Checkpoint03;
+	public MeshRenderer Checkpoint04;
+	public MeshRenderer Checkpoint05;
+	public MeshRenderer Checkpoint06;
+	public MeshRenderer Checkpoint07;
 
-	private void OnTriggerEnter(Collider other)
+	void Start(){
+		Checkpoint02.enabled = false;
+		Checkpoint03.enabled = false;
+		Checkpoint04.enabled = false;
+		Checkpoint05.enabled = false;
+		Checkpoint06.enabled = false;
+		Checkpoint07.enabled = false;
+	}
+
+	private void OnTriggerEnter(Collider collider)
 	{
 		GameObject.Find ("Car").SendMessage ("updateTime");
-		Checkpoint01.enabled = false;
+		switch (collider.gameObject.name) {
+			case "Checkpoint01":
+				Checkpoint01.enabled = false;
+				Checkpoint02.enabled = true;
+				Debug.Log (collider.gameObject.name);
+				break;
+			case "Checkpoint02":
+				Checkpoint02.enabled = false;
+				Checkpoint03.enabled = true;
+				Debug.Log (collider.gameObject.name);
+				break;
+			case "Checkpoint03":
+				Checkpoint03.enabled = false;
+				Checkpoint04.enabled = true;
+				Debug.Log (collider.gameObject.name);
+				break;
+			case "Checkpoint04":
+				Checkpoint04.enabled = false;
+				Checkpoint05.enabled = true;
+				Debug.Log (collider.gameObject.name);
+				break;
+			case "Checkpoint05":
+				Checkpoint05.enabled = false;
+				Checkpoint06.enabled = true;
+				Debug.Log (collider.gameObject.name);
+				break;
+			case "Checkpoint06":
+				Checkpoint06.enabled = false;
+				Checkpoint07.enabled = true;
+				Debug.Log (collider.gameObject.name);
+				break;
+			case "Checkpoint07":
+				Checkpoint07.enabled = false;
+				Debug.Log (collider.gameObject.name);
+				break;
+		}
 	}
+		
 }
