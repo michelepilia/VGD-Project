0509c4dbe5da9708d6ef902744d01f49f5dfa8f9
mzara10 <zaramassimo.mz@gmail.com>
2018-04-09 12:59:09 +0200

    merge dei progetti con l'aggiunta della scena Forest

---
+++ Assets/CarInWater.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CarInWater : MonoBehaviour {
+
+	public Rigidbody Car;
+
+	private void OnTriggerEnter(Collider collider)
+	{
+		if(collider.gameObject.name == "WaterProNighttime")
+		{
+			Car.drag = 5;
+			Debug.Log (collider.gameObject.name);
+		}
+	}
+}
