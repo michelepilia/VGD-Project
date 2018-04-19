a6fa5581317fd524c1bd7a3b72d8036a0c14cfe7
amarotto2 <marotz91@gmail.com>
2018-04-19 10:44:24 +0200

    migliorato il menu principale e abilitato il bottone Nuova Partita con scelta della difficolta'

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,18 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 
 public class MainMenu : MonoBehaviour {
 
+	public GameObject canvasMenuPrincipale;
+	public GameObject canvasDifficolta;
+
 	// Use this for initialization
 	void Start () {
-		
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+
+	}
+
+
+
+	public void NewGame()
+	{
+		canvasMenuPrincipale.SetActive(false);
+		canvasDifficolta.SetActive (true);
 	}
 
 	public void QuitGame()
---
+++ Assets/Scripts/SelectDifficulty.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SelectDifficulty : MonoBehaviour {
+
+	public GameObject canvasMenuPrincipale;
+	public GameObject canvasDifficolta;
+	public string gameDifficulty;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+
+	public void EasyButton()
+	{
+		gameDifficulty = "easy";
+		PlayerPrefs.SetString("gameDifficulty", gameDifficulty);
+		SceneManager.LoadScene ("SelectCar");
+	}
+
+	public void HardButton()
+	{
+		
+	}
+
+	public void BackButton()
+	{
+		canvasMenuPrincipale.SetActive(true);
+		canvasDifficolta.SetActive (false);
+	}
+}
