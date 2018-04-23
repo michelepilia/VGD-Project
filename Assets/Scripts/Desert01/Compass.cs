fef26574f190ef7a03f7233e684d2b4617d20f68
michele.pilia <michele.pilia@outlook.it>
2018-04-23 14:57:57 +0200

    inizio script bussola

---
+++ Assets/Scripts/Desert01/Compass.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Compass : MonoBehaviour {
+
+	public Vector3 NorthDirection;
+	public Transform Player;
+	public Quaternion MissionDirection;
+
+	public RectTransform NorthLayer;
+	public RectTransform MissionLayer;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ChangeNorthDirection ();
+		ChangeMissionDirection();
+		
+	}
+
+
+
+	void ChangeNorthDirection () {
+		NorthDirection.z = Player.eulerAngles.y;
+	
+	}
+
+
+	void ChangeMissionDirection() {
+
+
+
+	}
+}
