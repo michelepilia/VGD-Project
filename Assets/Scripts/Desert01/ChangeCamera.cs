2b1160cf07e4c9b8bd46c9bca7c3226baddc6105
michele.pilia <michele.pilia@outlook.it>
2018-04-26 10:22:59 +0200

    Aggiunto script ChangeCamera

---
+++ Assets/Scripts/Desert01/ChangeCamera.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeCamera : MonoBehaviour {
+
+	public Camera MainCamera;
+	public Camera HighCamera;
+	GameObject carSignaller;
+
+	// Use this for initialization
+	void Start () {
+
+		carSignaller = GameObject.FindGameObjectWithTag ("TargetMap");
+		MainCamera.enabled = true;
+		HighCamera.enabled = false;
+		carSignaller.SetActive (false);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.U)) {
+			MainCamera.enabled = !MainCamera.enabled;
+			HighCamera.enabled = !HighCamera.enabled;
+			carSignaller.SetActive (!(carSignaller.activeSelf));
+
+		}
+	}
+}
+
