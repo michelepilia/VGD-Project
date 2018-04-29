d47aeb7b6d52199ec7cb755dea7f44a591210279
michele.pilia <michele.pilia@outlook.it>
2018-04-29 17:32:53 +0200

    Corretto trigger acqua Desert

--- Assets/Scripts/WaterCollider.cs
+++ Assets/Scripts/WaterCollider.cs
@@ -7,7 +7,7 @@ using UnityEngine.SceneManagement;
 public class WaterCollider : MonoBehaviour {
 
 	public GameObject GameOverMenu;
-	public float timePause = 1.4f;
+	public float timePause = 1.2f;
 	//public enum Mode {Simple = 1, Hard = 2};
 	//public Mode mode = Mode.Simple;
 	public Rigidbody rb;
@@ -45,9 +45,10 @@ public class WaterCollider : MonoBehaviour {
 	}
 
 	IEnumerator Reposition() {
+		yield return new WaitForSeconds (0.2f);
 		rb.velocity = new  Vector3(0, 0, 0);
-		yield return new WaitForSeconds (timePause);
-		transform.position = new Vector3(565.05f, 20.0f, 888.0f); //1537, 0, 348
+		yield return new WaitForSeconds (1.0f);
 		transform.rotation = Quaternion.Euler (15.0f, 114.0f, 10.0f);
+		transform.position = new Vector3(565.05f, 20.0f, 888.0f); //1537, 0, 348
 	}
 }
